5bc5c75053c4f9177d3cc31dcf6d039d7f9353fc
misganawiw <41357881+misganawiw@users.noreply.github.com>
2022-11-25 10:52:35 +0300

    little bit more improvement to the main.dart

--- lib/try_widgets/navigationing/drawers.dart
+++ lib/try_widgets/navigationing/drawers.dart
@@ -2,15 +2,40 @@ import 'package:flutter/material.dart';
 
 
 class Drawerign extends StatelessWidget {
-  const Drawerign({super.key});
+   Drawerign({super.key});
+
+  List<String> xylem = [
+    "jimma",
+    "diredewa",
+    "addisabeba",
+    "adama",
+    "afar",
+    "jijiga",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Title'),
+        actions: [
+          
+        ],
       ),
-      body: Container(),
+      body: Container(
+
+          child:  ListView.builder(
+              itemCount: xylem.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                    leading:CircleAvatar(child:  Text(xylem[index][0])),
+                );
+              },
+
+          ),
+            
+
+      ),
       drawer: Drawer(
         child: ListView(
           children: const <Widget>[
@@ -21,6 +46,13 @@ class Drawerign extends StatelessWidget {
           ],
        ),
       ),
+      bottomNavigationBar: BottomNavigationBar(items: 
+      [
+        BottomNavigationBarItem(icon: Icon(Icons.home),label: "HOME",),
+        BottomNavigationBarItem(icon: Icon(Icons.invert_colors_on_sharp),label: "Asrat",),
+        BottomNavigationBarItem(icon: Icon(Icons.smart_display_rounded),label: "LEARN",),
+
+      ],),
     );;
   }
 }
